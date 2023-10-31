8f3982f21e93c7881de6e7578cd4bca21c8ae024
dhian haryono <dhianharyono.dh@gmail.com>
2023-10-31 15:27:36 +0700

    Add Project Section

---
+++ app/components/ProjectSection.jsx
@@ -0,0 +1,56 @@
+"use client"
+import React, { useState } from 'react'
+import ProjectCard from './ProjectCard'
+import { projectsData } from './utils'
+import ProjectTag from './ProjectTag'
+
+const ProjectSection = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Project
+      </h2>
+      <div className='text-white flex flex-white justify-center items-center gap-2 py-6'>
+        <ProjectTag
+          onClick={() => handleTagChange("All")}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={() => handleTagChange("Web")}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={() => handleTagChange("Mobile")}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+          {filteredProjects.map((project) => 
+              <ProjectCard
+                  key={project.id}
+                  title={project.title}
+                  description={project.description}
+                  imgUrl={project.image}
+                  gitUrl={project.gitUrl}
+                  previewUrl={project.previewUrl}
+              />
+          )}
+      </div>
+    </section>
+  )
+}
+
+export default ProjectSection
