44e440b80f2b3f1d50515614baae2777748e4c06
dhian haryono <dhianharyono.dh@gmail.com>
2023-11-06 14:01:33 +0700

    Update Route Email

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,27 +1,27 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-  const { body } = req; 
-  const {email, subject, message} = body;
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: ['dhianharyono.dh@gmail.com', email],
+      to: [fromEmail, email],
       subject: subject,
-      react: 
+      react: (
         <>
           <h1>{subject}</h1>
-          <p>Thankyou for contacting us!</p>
-          <p>New message submit</p>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
           <p>{message}</p>
         </>
-      });
-
+      ),
+    });
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error });
